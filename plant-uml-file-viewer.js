1739f15f35fa092c92916ab3a35bd6f66eb9a2d6
Kyle Carter <kylec32@gmail.com>
2019-10-01 21:43:47 -0600

    Initial commit

---
+++ plant-uml-file-viewer.js
@@ -0,0 +1,41 @@
+function markupChanged() {
+    let markupElement = document.getElementById("markup");
+
+    let compressedData = compress(markupElement.value);
+    fetch("https://www.plantuml.com/plantuml/svg/" + compressedData)
+        .then(value => value.text()
+        .then(text => {
+            let svgBox = document.getElementById("diagram");
+            svgBox.innerHTML = text;
+            }
+        ));
+}
+
+var returnedFunction = debounce(function() {
+    markupChanged()
+  }, 300);
+
+let markupElement = document.getElementById("markup");
+markupElement.addEventListener('keyup', returnedFunction);
+
+function debounce(func, wait, immediate) {
+    var timeout;
+
+    return function executedFunction() {
+      var context = this;
+      var args = arguments;
+
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+  
+      var callNow = immediate && !timeout;
+
+      clearTimeout(timeout);
+
+      timeout = setTimeout(later, wait);
+
+      if (callNow) func.apply(context, args);
+    };
+  };
